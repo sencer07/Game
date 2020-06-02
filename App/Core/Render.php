d7bf40e0deb1498e5df7b24f952cb490cd885766
ruben <room3622@gmail.com>
2020-06-02 16:59:58 +0100

    Update

---
+++ App/Core/Render.php
@@ -0,0 +1,60 @@
+<?php
+
+
+class Render
+{
+
+
+    public function Json($data = null)
+    {
+        header('Content-Type: application/json, text/javascript');
+        echo json_encode($data);
+        exit();
+    }
+
+
+    public static function viewsNoexit($file = null, $data = null)
+    {
+
+
+        $file_dir = Sys() . 'Viwes/' . $file . '.php';
+        if (file_exists($file_dir)) {
+
+            include($file_dir);
+
+        } else {
+            echo "<div align='center'>
+    <h1>This File <b style='color: red'>" . ucfirst($file) . "</b> that don't exist</h1><br/>
+    <h2>On <b style='color: red' >Viwes</b></h2>
+</div>";
+            exit();
+        }
+
+
+    }
+
+
+    public static function views($file = null, $data = null)
+    {
+
+
+        $file_dir = Sys() . 'Viwes/' . $file . '.php';
+        if (file_exists($file_dir)) {
+
+            include($file_dir);
+
+            exit();
+
+        } else {
+            echo "<div align='center'>
+    <h1>This File <b style='color: red'>" . ucfirst($file) . "</b> that don't exist</h1><br/>
+    <h2>On <b style='color: red' >Viwes</b></h2>
+</div>";
+            exit();
+        }
+
+
+    }
+
+
+}
--- initialize.php
+++ initialize.php
@@ -1,1 +1,101 @@
 <?php
+ob_start();
+
+function Sys()
+{
+    $path = __DIR__ . "/";
+    $sys = $path . "App/";
+    return $sys;
+}
+
+function Composer()
+{
+    $Composer = Sys() . "Composer/vendor/autoload.php";
+    if (file_exists($Composer)) {
+        require_once $Composer;
+
+    }
+}
+
+Composer();
+
+
+spl_autoload_register("loader");
+
+function loader($classname)
+{
+
+    $exe = ".php";
+    $fullpath1 = Sys() . "Controllers/" . $classname . $exe;
+    $fullpath2 = Sys() . "Core/" . $classname . $exe;
+
+    if (file_exists($fullpath1)) {
+
+        require $fullpath1;
+
+    } elseif (file_exists($fullpath2)) {
+
+
+        require $fullpath2;
+
+    } else {
+
+        return false;
+    }
+
+
+}
+
+
+
+function Devent(){
+
+    $Devent =  __DIR__ . "/.env";
+
+
+    if(!file_exists($Devent)){
+
+
+        //$homepage = file_get_contents(__DIR__ . '/.env.example');
+
+        $myfile = fopen($Devent, "w") or die("Unable to open file!");
+        $txt = file_get_contents(__DIR__. '/.env.example', true);
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+    }
+
+
+
+}
+
+Devent();
+
+
+
+$session = new Session();
+
+
+if ($session->is_logged_in()) {
+    $session->user_id;
+
+
+    if (isset($_GET['module'])) {
+        $file_dir = Sys() . 'Modules/' . ucfirst($_GET['module']) . '.php';
+
+        echo $file_dir;
+
+        if (file_exists($file_dir)) {
+            include $file_dir;
+        } else {
+            echo "<div align='center'>
+    <h1>This Module <b style='color: red'>" . ucfirst($_GET['module']) . "</b> that don't exist</h1>
+</div>";
+            exit();
+        }
+    }
+
+}
+
+
+ob_end_flush();
